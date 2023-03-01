cef2ccca56e46fd93c3c7b38f3a029ec7a46eb31
pv-kiran <kiranpv903@gmail.com>
2023-03-01 18:24:36 +0530

    Looping an list of elements

--- src/APPtwo.js
+++ src/APPtwo.js
@@ -3,13 +3,13 @@ import React from 'react'
 function BookList({title , author , imgSrc , children}) {
   
   console.log(title , author , imgSrc , children);
- 
   return (
     <article className='bookContainer'>
-      <img src= {imgSrc} alt="Book_One"/>
+      {children}
+      {/* <img src= {imgSrc} alt="Book_One"/>
       <h1>{title.toUpperCase()}</h1>
       <h1>{author}</h1>
-      {children}
+      {children} */}
     </article>
   )
 }
--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import BookList from './APPtwo';
 
 
 function App() {
-  const myBook = {
-    title: `World’s Best Inspirational Books to Change Your Life` ,
-    author: `Joseph Murphy Dale Carnegie, Napoleon Hill` ,
-    imgSrc: `https://m.media-amazon.com/images/I/81BfYDO0kSL._AC_UY327_FMwebp_QL65_.jpg`
-  }
-  
+   const books = [
+    {
+      title: `World’s Best Inspirational Books to Change Your Life` ,
+      author: `Joseph Murphy Dale Carnegie, Napoleon Hill` ,
+      imgSrc: `https://m.media-amazon.com/images/I/81BfYDO0kSL._AC_UY327_FMwebp_QL65_.jpg`
+    } ,
+    {
+      title:  `Our Non-fairy Tale Life (Order Now & Get Author Signed Copy)` ,
+      author: `by Rooprashi` ,
+      imgSrc: `https://m.media-amazon.com/images/I/71aCBltKcqL._AC_UY327_FMwebp_QL65_.jpg`
+    }
+  ]  
+  const name = ['Kiran' , 'Arun']
+  const newName = name.map((name) => {
+    return <h1>{name}</h1>
+  })
+  console.log(newName);
   return (
-    <BookList {...myBook}>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, inventore?</p>
-    </BookList>
+    <React.Fragment>
+      <BookList > {newName} </BookList>
+    </React.Fragment>
+    
   );
 }
 
