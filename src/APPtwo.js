28999c35994a8916a05fb3cc652838c3acbaf656
pv-kiran <kiranpv903@gmail.com>
2023-03-01 19:58:55 +0530

    Event handling in react

--- src/APPtwo.js
+++ src/APPtwo.js
@@ -4,11 +4,23 @@ function BookList({title , author , imgSrc }) {
 
 //   const {title , author , imgSrc } = props.book; 
 
+
+  const handleClick = (e) => {
+    console.log(e.target.textContent);
+    alert('Hello World');
+  }
+
+  const handleComplex = (author) => {
+    alert(author);
+  }
+  
   return (
     <article className='bookContainer'>
       <img src= {imgSrc} alt="Book_One"/>
-      <h1>{title.toUpperCase()}</h1>
+      <h1 onClick={(e) => handleClick(e)}>{title.toUpperCase()}</h1>
       <h1>{author}</h1>
+      <button onClick={handleClick}>Save</button>
+      <button onClick={() => handleComplex(author)}>Complex</button>
     </article>
   )
 }
